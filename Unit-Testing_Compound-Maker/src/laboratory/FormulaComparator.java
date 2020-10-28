d8e256c830d5ba92dc9773c9463e68140945e050
Luke Edmondson <luke_ed@outlook.com>
2020-10-28 12:24:47 -0400

    Removed Project 1 Files and Fragments that shouldn't be included

--- Unit-Testing_Compound-Maker/src/laboratory/FormulaComparator.java
+++
@@ -1,9 +0,0 @@
-package laboratory;
-//
-import java.util.Comparator;
-
-public class FormulaComparator implements Comparator<CompoundElement> {
-  public int compare (CompoundElement compound1, CompoundElement compound2){
-    return compound1.getFormula().compareTo(compound2.getFormula());
-  }
-}
