6193d1d173110828237dd504ef051ce805abf563
Luke-Ed <luke_ed@outlook.com>
2020-10-31 20:48:35 -0400

    Started working on fixing some of the issues regarding the logout window.

--- Project-2-EMS/NurseView.xaml.cs
+++ Project-2-EMS/NurseView.xaml.cs
@@ -1,9 +1,23 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace Project_2_EMS {
   /// <summary>
-  ///   Interaction logic for NurseView.xaml
+  /// Interaction logic for NurseView.xaml
   /// </summary>
   public partial class NurseView {
     private readonly Window _parentWindow;
@@ -15,15 +29,14 @@ namespace Project_2_EMS {
     }
 
     private void LogOutButton_Click(object sender, RoutedEventArgs e) {
-      Hide();
-      var mainWindow = _parentWindow;
+      Window mainWindow = _parentWindow;
       mainWindow.Show();
+      Close();
     }
 
     private void OnWindowClosing(object sender, CancelEventArgs e) {
-      var mainWindow = _parentWindow;
+      Window mainWindow = _parentWindow;
       mainWindow.Close();
-      Close();
     }
   }
 }
