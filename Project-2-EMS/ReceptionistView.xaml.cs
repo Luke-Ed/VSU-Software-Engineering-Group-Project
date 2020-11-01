9410a28a734db595fc4e50794e864d440ca49a8c
Ryan Slaybaugh <rgslaybaugh@valdosta.edu>
2020-10-31 23:42:40 -0400

    Removed ability for user to select appointments on weekends.

--- Project-2-EMS/ReceptionistView.xaml.cs
+++ Project-2-EMS/ReceptionistView.xaml.cs
@@ -68,9 +68,20 @@ namespace Project_2_EMS{
         // This region holds the code/event handlers for Calendar view
         #region
         private void ApptCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e) {
-          if (ApptCalendar.SelectedDate.HasValue) {
-              AppointmentDate.Content = ApptCalendar.SelectedDate.Value.ToString("MM/dd/yyyy");
-          }
+            if (ApptCalendar.SelectedDate.HasValue) {
+                AppointmentDate.Content = ApptCalendar.SelectedDate.Value.ToString("MM/dd/yyyy");
+                AppointmentDate.Content = ApptCalendar.SelectedDate.Value.ToString("dddd");
+                if (ApptCalendar.SelectedDate.Value.ToString("dddd").Equals("Saturday") || ApptCalendar.SelectedDate.Value.ToString("dddd").Equals("Sunday"))
+                {
+                    ApptDateViewer.Visibility = Visibility.Hidden;
+                    ApptDateViewerEmpty.Visibility = Visibility.Visible;
+                }
+                else {
+                    ApptDateViewerEmpty.Visibility = Visibility.Hidden;
+                    ApptDateViewer.Visibility = Visibility.Visible;
+                }
+            }
+            PatientQuickViewLabel.Content = "No appointment selected";
         }
 
         private void ApptCalendar_GotMouseCapture(object sender, MouseEventArgs e) {
@@ -84,59 +95,59 @@ namespace Project_2_EMS{
         }
 
         private void Appt1_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "9:00 appointment available";
         }
 
         private void Appt2_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "9:30 appointment available";
         }
 
         private void Appt3_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "10:00 appointment available";
         }
 
         private void Appt4_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "10:30 appointment available";
         }
 
         private void Appt5_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "11:00 appointment available";
         }
 
         private void Appt6_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "11:30 appointment available";
         }
 
         private void Appt7_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "1:00 appointment available";
         }
 
         private void Appt8_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "1:30 appointment available";
         }
 
         private void Appt9_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "2:00 appointment available";
         }
 
         private void Appt10_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "2:30 appointment available";
         }
 
         private void Appt11_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "3:00 appointment available";
         }
 
         private void Appt12_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "3:30 appointment available";
         }
 
         private void Appt13_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "4:00 appointment available";
         }
 
         private void Appt14_Click(object sender, RoutedEventArgs e) {
-
+            PatientQuickViewLabel.Content = "4:30 appointment available";
         }
         #endregion
     }
