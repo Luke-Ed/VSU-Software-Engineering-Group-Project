d2f8890cd960f192a4184929b61363905f793d62
Ryan Slaybaugh <rgslaybaugh@valdosta.edu>
2020-12-02 18:36:28 -0500

    Experimentation with formatting timespan datatype and relocated the sql code for appointmnets.

--- Project-2-EMS/App_Code/ReceptionSqlHandler.cs
+++ Project-2-EMS/App_Code/ReceptionSqlHandler.cs
@@ -10,7 +10,7 @@ namespace Project_2_EMS.App_Code
     {
         public string AppointmentQuerier(DateTime apptDate)
         {
-            String query = "SELECT * FROM Appointments WHERE ApptDate = '" + apptDate + "'";
+            String query = "SELECT * FROM Appointments WHERE ApptDate BETWEEN '" + apptDate + "' AND  '" + apptDate.AddDays(6) + "'";
             return query;
         }
     }
--- Project-2-EMS/ReceptionistView.xaml.cs
+++ Project-2-EMS/ReceptionistView.xaml.cs
@@ -1,219 +1,257 @@
-﻿using System;
+﻿using Project_2_EMS.App_Code;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 
-using System.Configuration;
-using System.Data.SqlClient;
+namespace Project_2_EMS
+{
+    public partial class ReceptionistView : Window
+    {
+        private readonly Window _parentWindow;
+        private Window newApptWindow;
+        SqlConnection connection;
+        private DateTime prevDate;
+        private DateTime weekDate;
+        private DateTime prevWeekDate;
 
-using Project_2_EMS.App_Code;
+        public ReceptionistView(Window parentWindow)
+        {
+            _parentWindow = parentWindow;
+            InitializeComponent();
+            InitializeDBConnection();
 
-namespace Project_2_EMS {
-  public partial class ReceptionistView : Window {
-    private readonly Window _parentWindow;
-    private Window newApptWindow;
-    SqlConnection connection;
-    private DateTime prevDate;
-    private DateTime weekDate;
+            weekDate = DateTime.Now.AddDays(Convert.ToDouble(DateTime.Now.DayOfWeek.ToString("d")) * -1.0);
+            AppointmentWeek.Content = weekDate.ToString("Week o\\f MMMM dd, yyyy");
 
-    public ReceptionistView(Window parentWindow) {
-      _parentWindow = parentWindow;
-      InitializeComponent();
-      InitializeDBConnection();
+            Closing += OnWindowClosing;
+        }
 
-      weekDate = DateTime.Now.AddDays(Convert.ToDouble(DateTime.Now.DayOfWeek.ToString("d")) * -1.0);
-      AppointmentWeek.Content = weekDate.ToString("Week o\\f MMMM dd, yyyy");
-
-      Closing += OnWindowClosing;
-    }
-
-        private void InitializeDBConnection() {
+        private void InitializeDBConnection()
+        {
             String connectionString = ConfigurationManager.ConnectionStrings["MDR_ConnStr"].ConnectionString;
             connection = new SqlConnection(connectionString);
             connection.Open();
         }
 
-    private void LogOutButton_Click(object sender, RoutedEventArgs e) {
-      Hide();
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
 
-      if (newApptWindow != null) newApptWindow.Close();
-
-            connection.Close();
-
-      var mainWindow = _parentWindow;
-      mainWindow.Show();
-    }
-
-    private void OnWindowClosing(object sender, CancelEventArgs e) {
-      var mainWindow = _parentWindow;
-
-      if (newApptWindow != null) newApptWindow.Close();
+            if (newApptWindow != null) newApptWindow.Close();
 
             connection.Close();
 
-      mainWindow.Close();
-    }
-
-    // Change which view is visible when you select buttons from the control panel
-      private void ControlButton_Click(object sender, RoutedEventArgs e) {
-        List<UIElement> views = GetChildren(ViewPanel);
-        Button btn = e.Source as Button;
-        foreach (Grid grid in views) {
-          _ = grid.Name.Contains(btn.Name) ? grid.Visibility = Visibility.Visible : grid.Visibility = Visibility.Hidden;
+            var mainWindow = _parentWindow;
+            mainWindow.Show();
         }
-        ApptButtonGrid.Visibility = Visibility.Hidden;
-      }
 
-    // Change the displayed date when you select a date on the calendar gui, highlight the day on the appointments calendar
-    private void ApptCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e) {
-      if (ApptCalendar.SelectedDate.HasValue) {
-        var date = ApptCalendar.SelectedDate.Value;
-        var dayNum = Convert.ToDouble(ApptCalendar.SelectedDate.Value.DayOfWeek.ToString("d"));
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            var mainWindow = _parentWindow;
 
-        weekDate = date.AddDays(dayNum * -1.0);
-        AppointmentWeek.Content = weekDate.ToString("Week o\\f MMMM dd, yyyy");
-        if (prevDate != date) {
-          prevDate = date;
-          var apptDays = GetChildren(AppointmentDays);
-          HighlightDay(apptDays, 0, (int) dayNum + 1);
+            if (newApptWindow != null) newApptWindow.Close();
 
+            connection.Close();
+
+            mainWindow.Close();
         }
-      }
-    }
 
-    // Code obtained from https://stackoverflow.com/questions/25352961/have-to-click-away-twice-from-calendar-in-wpf
-    /**
-     *  When clicking inside the calendar view, you would need to double click outside of it before being able
-     *  to click on something outside of it. This code prevents that from happening
-     */ 
-    private void ApptCalendar_GotMouseCapture(object sender, MouseEventArgs e) {
-      UIElement originalElement = e.OriginalSource as UIElement;
-      if (originalElement is CalendarDayButton || originalElement is CalendarItem) {
-          originalElement.ReleaseMouseCapture();
-      }
-    }
-    // Return a list of the given grid's children
-    private static List<UIElement> GetChildren(Grid grid) {
-      List<UIElement> children = new List<UIElement>();
-      foreach (UIElement child in grid.Children){
-        children.Add(child);
-      }
-      return children;
-    }
-
-    // Get individual child from UIElement
-    private static UIElement GetChild(Grid grid, int row, int column) {
-      foreach (UIElement child in grid.Children){
-        if (Grid.GetRow(child) == row && Grid.GetColumn(child) == column){
-          return child;
+        // Change which view is visible when you select buttons from the control panel
+        private void ControlButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<UIElement> views = GetChildren(ViewPanel);
+            Button btn = e.Source as Button;
+            foreach (Grid grid in views)
+            {
+                _ = grid.Name.Contains(btn.Name) ? grid.Visibility = Visibility.Visible : grid.Visibility = Visibility.Hidden;
+            }
+            ApptButtonGrid.Visibility = Visibility.Hidden;
         }
-      }
-      return null;
-    }
 
-    // Highlight the selected day on the appointments calendar
-    private static void HighlightDay(List<UIElement> days, int row, int column) {
-      foreach (Label l in days) {
-        if (Grid.GetRow(l) == row && Grid.GetColumn(l) == column) {
-          var bc = new BrushConverter();
-          l.Background = (Brush)bc.ConvertFrom("#FF6BBBBD");
+        // Change the displayed date when you select a date on the calendar gui, highlight the day on the appointments calendar
+        private void ApptCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ApptCalendar.SelectedDate.HasValue)
+            {
+                var date = ApptCalendar.SelectedDate.Value;
+                var dayNum = Convert.ToDouble(ApptCalendar.SelectedDate.Value.DayOfWeek.ToString("d"));
+
+                weekDate = date.AddDays(dayNum * -1.0);
+                AppointmentWeek.Content = weekDate.ToString("Week o\\f MMMM dd, yyyy");
+
+                if (prevDate != date)
+                {
+                    prevDate = date;
+                    var apptDays = GetChildren(AppointmentDays);
+                    HighlightDay(apptDays, 0, (int)dayNum + 1);
+                }
+
+                if (prevWeekDate != weekDate)
+                {
+                    prevWeekDate = weekDate;
+
+                    ReceptionSqlHandler rcsql = new ReceptionSqlHandler();
+                    string query = rcsql.AppointmentQuerier(date);
+
+                    SqlCommand cmd = new SqlCommand { Connection = connection, CommandText = query };
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+
+                    List<PatientAppointment> patAppts = new List<PatientAppointment>();
+                    PatientAppointment pa = null;
+
+                    while (dataReader.Read())
+                    {
+                        int visitId = dataReader.GetInt32(0);
+                        int patientId = dataReader.GetInt32(1);
+                        DateTime apptDate = dataReader.GetDateTime(2);
+                        TimeSpan apptTime = dataReader.GetTimeSpan(3);
+                        decimal cost = dataReader.GetDecimal(4);
+                        string receptNote = dataReader.GetString(5);
+                        string nurseNote = dataReader.GetString(6);
+                        string doctorNote = dataReader.GetString(7);
+
+                        pa = new PatientAppointment(visitId, patientId, apptDate, apptTime, cost, receptNote, nurseNote, doctorNote);
+                        patAppts.Add(pa);
+                    }
+
+                    if (patAppts.Count > 0)
+                        MessageBox.Show(patAppts.Count.ToString());
+                    if (patAppts.Count > 0)
+                        MessageBox.Show(string.Format("{0:hh\\:mm}", patAppts.ElementAt(0).ApptTime));
+
+                    dataReader.Close();
+                }
+            }
         }
-        else {
-          l.Background = Brushes.LightCyan;
+
+        // Code obtained from https://stackoverflow.com/questions/25352961/have-to-click-away-twice-from-calendar-in-wpf
+        /**
+         *  When clicking inside the calendar view, you would need to double click outside of it before being able
+         *  to click on something outside of it. This code prevents that from happening
+         */
+        private void ApptCalendar_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            UIElement originalElement = e.OriginalSource as UIElement;
+            if (originalElement is CalendarDayButton || originalElement is CalendarItem)
+            {
+                originalElement.ReleaseMouseCapture();
+            }
         }
-      }
-    }
 
-    // Highlight the selected cell on the appointments calendar     
-    private static void HighlightAppointment(Grid grid, int row, int column) {
-      foreach (Label child in grid.Children) {
-        _ = Grid.GetRow(child) == row && Grid.GetColumn(child) == column ? child.Margin = new Thickness(2) : child.Margin = new Thickness(0.5);
-      }
-    }
+        // Return a list of the given grid's children
+        private static List<UIElement> GetChildren(Grid grid)
+        {
+            List<UIElement> children = new List<UIElement>();
+            foreach (UIElement child in grid.Children)
+            {
+                children.Add(child);
+            }
+            return children;
+        }
 
-    // Called when a cell on the appointments calendar is selected
-    private void ApptDate_MouseDown(object sender, MouseButtonEventArgs e){
-      // Highlight the selected cell
-      Label srcLabel = e.Source as Label;
-      HighlightAppointment(AppointmentGrids, Grid.GetRow(srcLabel), Grid.GetColumn(srcLabel));
-      
-      // Highlight the day corresponding to the selected cell
-      List<UIElement> apptDays = GetChildren(AppointmentDays);
-      HighlightDay(apptDays, 0, Grid.GetColumn(srcLabel) + 2);
+        // Get individual child from UIElement
+        private static UIElement GetChild(Grid grid, int row, int column)
+        {
+            foreach (UIElement child in grid.Children)
+            {
+                if (Grid.GetRow(child) == row && Grid.GetColumn(child) == column)
+                {
+                    return child;
+                }
+            }
+            return null;
+        }
 
-      // Show the selected date on the calendar view
-      DateTime date = weekDate.AddDays(Grid.GetColumn(srcLabel) + 1);
-      ApptCalendar.SelectedDate = date;
+        // Highlight the selected day on the appointments calendar
+        private static void HighlightDay(List<UIElement> days, int row, int column)
+        {
+            foreach (Label l in days)
+            {
+                if (Grid.GetRow(l) == row && Grid.GetColumn(l) == column)
+                {
+                    var bc = new BrushConverter();
+                    l.Background = (Brush)bc.ConvertFrom("#FF6BBBBD");
+                }
+                else
+                {
+                    l.Background = Brushes.LightCyan;
+                }
+            }
+        }
 
-      // Show a new/view appointment button whenver a cell is selected
-      ApptButtonGrid.Visibility = Visibility.Visible;
-      _ = srcLabel.Content.ToString() == String.Empty ? ViewApptButton.Content = "New Appointment" : ViewApptButton.Content = "View Appointment";
-    }
+        // Highlight the selected cell on the appointments calendar     
+        private static void HighlightAppointment(Grid grid, int row, int column)
+        {
+            foreach (Label child in grid.Children)
+            {
+                _ = Grid.GetRow(child) == row && Grid.GetColumn(child) == column ? child.Margin = new Thickness(2) : child.Margin = new Thickness(0.5);
+            }
+        }
 
-    // Called when a cell on the appointments calendar has been double clicked
-    private void ApptDate_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
-      if (newApptWindow != null) newApptWindow.Close();
+        // Called when a cell on the appointments calendar is selected
+        private void ApptDate_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Highlight the selected cell
+            Label srcLabel = e.Source as Label;
+            HighlightAppointment(AppointmentGrids, Grid.GetRow(srcLabel), Grid.GetColumn(srcLabel));
 
-      Label srcLabel = e.Source as Label;
+            // Highlight the day corresponding to the selected cell
+            List<UIElement> apptDays = GetChildren(AppointmentDays);
+            HighlightDay(apptDays, 0, Grid.GetColumn(srcLabel) + 2);
 
+            // Show the selected date on the calendar view
+            DateTime date = weekDate.AddDays(Grid.GetColumn(srcLabel) + 1);
+            ApptCalendar.SelectedDate = date;
+
+            // Show a new/view appointment button whenver a cell is selected
+            ApptButtonGrid.Visibility = Visibility.Visible;
+            _ = srcLabel.Content.ToString() == String.Empty ? ViewApptButton.Content = "New Appointment" : ViewApptButton.Content = "View Appointment";
+        }
+
+        // Called when a cell on the appointments calendar has been double clicked
+        private void ApptDate_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (newApptWindow != null) newApptWindow.Close();
+
+            Label srcLabel = e.Source as Label;
+
+            OpenAppointmentView(srcLabel);
+        }
+
+        private void ViewApptButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (newApptWindow != null)
+            {
+                newApptWindow.Close();
+            }
+
+            Label srcLabel = null;
+            Thickness thc = new Thickness(2);
+
+            foreach (Label child in AppointmentGrids.Children)
+            {
+                _ = child.Margin.Equals(thc) ? srcLabel = child : null;
+            }
+
+            OpenAppointmentView(srcLabel);
+        }
+
+        private void OpenAppointmentView(Label srcLabel)
+        {
+            Label timeLabel = GetChild(AppointmentTimes, Grid.GetRow(srcLabel), 0) as Label;
             DateTime date = weekDate.AddDays(Grid.GetColumn(srcLabel) + 1);
 
-            ReceptionSqlHandler rcsql = new ReceptionSqlHandler();
-            string query = rcsql.AppointmentQuerier(date);
-
-            SqlCommand cmd = new SqlCommand { Connection = connection, CommandText = query };
-            SqlDataReader dataReader = cmd.ExecuteReader();
-
-            PatientAppointment pa = null;
-
-            while (dataReader.Read())
-            {
-                int visitId = dataReader.GetInt32(0);
-                int patientId = dataReader.GetInt32(1);
-                DateTime apptDate = dataReader.GetDateTime(2);
-                TimeSpan apptTime = dataReader.GetTimeSpan(3);
-                decimal cost = dataReader.GetDecimal(4);
-                string receptNote = dataReader.GetString(5);
-                string nurseNote = dataReader.GetString(6);
-                string doctorNote = dataReader.GetString(7);
-
-                pa = new PatientAppointment(visitId, patientId, apptDate, apptTime, cost, receptNote, nurseNote, doctorNote);
-            }
-            
-            if (pa != null)
-                MessageBox.Show(pa.NurseNote);
-
-            dataReader.Close();
-
-            //OpenAppointmentView(srcLabel);
+            //ToString("ddd dd, yyyy")
+            newApptWindow = new NewAppointmentWindow(srcLabel, timeLabel, date);
+            newApptWindow.Show();
         }
-
-    private void ViewApptButton_Click(object sender, RoutedEventArgs e) {
-      if (newApptWindow != null) {
-        newApptWindow.Close();
-      }
-
-      Label srcLabel = null;
-      Thickness thc = new Thickness(2);
-
-      foreach (Label child in AppointmentGrids.Children) {
-        _ = child.Margin.Equals(thc) ? srcLabel = child : null;
-      }
-
-      OpenAppointmentView(srcLabel);
     }
-
-    private void OpenAppointmentView(Label srcLabel) {
-      Label timeLabel = GetChild(AppointmentTimes, Grid.GetRow(srcLabel), 0) as Label;
-      DateTime date = weekDate.AddDays(Grid.GetColumn(srcLabel) + 1);
-
-      //ToString("ddd dd, yyyy")
-      newApptWindow = new NewAppointmentWindow(srcLabel, timeLabel, date);
-      newApptWindow.Show();
-    }
-  }
 }
