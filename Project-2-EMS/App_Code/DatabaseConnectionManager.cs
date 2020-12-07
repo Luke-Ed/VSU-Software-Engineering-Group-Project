dca73e4da0f5bdf8fbbb714569a948fdb0cea774
Ryan Slaybaugh <rgslaybaugh@valdosta.edu>
2020-12-06 19:37:09 -0500

    Added a database manager to handle database connection and changed my code to use parameterization when querying the database.

---
+++ Project-2-EMS/App_Code/DatabaseConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2_EMS.App_Code
+{
+    public class DatabaseConnectionManager
+    {
+        public DatabaseConnectionManager() {
+        }
+
+        private string GetDBConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["MDR_ConnStr"].ConnectionString;
+        }
+
+        public SqlConnection ConnectToDatabase()
+        {
+            return new SqlConnection(GetDBConnectionString());
+        }
+    }
+}
